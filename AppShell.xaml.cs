bc0b7cdbcd529222963c1762ae159add6550682d
Erick Rosero <roseroerick15@gmail.com>
2023-07-03 08:53:29 -0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ER_Apuntes;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute(nameof(Views.ER_NotePage), typeof(Views.ER_NotePage));
+    }
+
+}
